23f6ee79606e73fc85affc6cdab0cbb0370ad30a
Vaggelis Yfantis <me@octoper.me>
2021-07-23 14:43:53 +0300

    fix tests

--- tests/HtmlMinifyTest.php
+++ tests/HtmlMinifyTest.php
@@ -2,46 +2,10 @@
 
 namespace Octoper\HtmlMinify\Tests;
 
-use Illuminate\Support\Facades\Route;
-use Octoper\HtmlMinify\HtmlMinifyMiddleware;
-use Statamic\Statamic;
 use function PHPUnit\Framework\assertEquals;
 
 class HtmlMinifyTest extends TestCase
 {
-    /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return void
-     */
-    protected function defineEnvironment($app)
-    {
-        $app['config']->set('html-minify.optimizeViaHtmlDomParser', true);
-
-        // Workaround for registering routes for tests
-        Statamic::booted(function () {
-            Statamic::pushWebRoutes(function () {
-                Route::namespace('\\Octoper\\HtmlMinify\\\Http\\Controllers')->group(function () {
-                    Route::get('/html-minify/test', function () {
-                        return <<<'HTML'
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-    <p>Hello</p>
-</body>
-</html>
-HTML;
-                    })->middleware(HtmlMinifyMiddleware::class);
-                });
-            });
-        });
-    }
-
     /** @test */
     public function check_if_can_minify_html_with_middleware()
     {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,9 @@
 namespace Octoper\HtmlMinify\Tests;
 
 
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Octoper\HtmlMinify\HtmlMinifyMiddleware;
 use Octoper\HtmlMinify\HtmlMinifyServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Statamic\Extend\Manifest;
@@ -32,7 +35,7 @@ abstract class TestCase extends OrchestraTestCase
 
         $app->make(Manifest::class)->manifest = [
             'statamic-html-minify' => [
-                'id'        => 'octoper/statamic-html-minify',
+                'id' => 'octoper/statamic-html-minify',
                 'namespace' => 'Octoper\\StatamicHtmlMinify',
             ]
         ];
@@ -48,9 +51,42 @@ abstract class TestCase extends OrchestraTestCase
         ];
 
         foreach ($configs as $config) {
-            $app['config']->set("statamic.$config", require(__DIR__."/../vendor/statamic/cms/config/{$config}.php"));
+            $app['config']->set("statamic.$config", require(__DIR__ . "/../vendor/statamic/cms/config/{$config}.php"));
         }
 
         $app['config']->set('statamic.users.repository', 'file');
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param Application $app
+     * @return void
+     */
+    protected function defineEnvironment($app)
+    {
+        $app['config']->set('html-minify.optimizeViaHtmlDomParser', true);
+
+        // Workaround for registering routes for tests
+        Statamic::booted(function () {
+            Statamic::pushWebRoutes(function () {
+                Route::namespace('\\Octoper\\HtmlMinify\\\Http\\Controllers')->group(function () {
+                    Route::get('/html-minify/test', function () {
+                        return <<<'HTML'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <p>Hello</p>
+</body>
+</html>
+HTML;
+                    })->middleware(HtmlMinifyMiddleware::class);
+                });
+            });
+        });
     }
 }
